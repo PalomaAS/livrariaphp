3890522943a5bcf6f7741bc14b9be142613dbf0f
Paloma <105325830+PalomaAS@users.noreply.github.com>
2022-06-16 17:51:53 -0300

    Add files via upload
    Arquivos do projeto livrariaphp

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+define('BD_SERVIDOR', 'localhost');
+define('BD_USUARIO','id19119221_palomalivraria');
+define('BD_SENHA','0CnVi>){e9Zl$E\*');
+define('BD_BANCO','id19119221_livrariaphpweb');
